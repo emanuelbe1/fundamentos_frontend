c34e3f301c579f21b25b50f5f8d926a34c24ce32
Emanuel B <emanuelbe3@gmail.com>
2023-01-04 20:10:23 -0300

    etc

--- 5-Fundamentos_programacion/variables.js
+++ 5-Fundamentos_programacion/variables.js
@@ -1,3 +1,6 @@
 let length = 16; //Int
+console.log(length)
 let lastName = "Benito" //Str
-let x = {nombre: "Emanuel", apellido:"Benito"} //obj 
+console.log(lastName)
+let dict = {nombre: "Emanuel", apellido:"Benito"} //obj 
+console.log(dict)
