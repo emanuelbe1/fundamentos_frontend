22ab3f95f38aa188b14c8cb563f57c0c5acfc1af
Emanuel B <emanuelbe3@gmail.com>
2023-01-05 19:05:39 -0300

    Nuevos

---
+++ 6_proyecto_final_integrador_front_end/er_data.js
@@ -0,0 +1,45 @@
+/*Nombre: Eugenio Rodriguez
+Correo: eugenio.rodriguez@example.com
+Fecha de nacimiento: 05/05/1988
+Dirección: 9386 Oak Ridge Ln
+Telefono: (371) 839-4702
+Contraseña: smoke1
+
+Resumen:
+
+ I am a hard worker who always try to do as had tailduands. I love reached in sales, high-peducation and productic. My application strategies more about my goal from them.
+
+Experiencia: 
+Project Manager And Sales Associate
+Alliance 2017 - Actual. Winston-Salem, North Carolina
+ Alliance is one of the leading performance of the consumer e-commerce company in the United Nations having to the best industrial services company in the world.
+ ·Achieved a 2-year strategy to increase swiscouragence and profit activities for fore and project management teams to achieve community of the store program and developed a successful content for the marketing company
+ ·Led a team of 100 people and increased risk of ranking to 10 million dollars in 2017 of the top 25 company partners
+ ·Led internal and successful marketing campaigns and increase in overall productivity
+ ·Completed 12 students and 2 events and 10 different support teams.
+ ·Increased the company to 500 percent in 7 years
+
+Project Geologist - Front Office
+Social American 2014-2017 en Wiston-Salem, North Carloina
+
+ Social American is automation tools and consulting schools and sales and services in the United Kingdom for health and strategy and experience in the field of students of the country and technical products and services in India. They are a production of mobile explorations and servicing services.
+
+·Managed 20 partnership consultants and marketing consultants.
+
+·Managed a team of 10 engineers and achieved a top 5 per annum initiative
+
+·Managed 2 new private projects in the first 12 markets in the US and Service.
+
+·Managed 5 projects at a service contract with a 50 percent plan in 2016
+
+·Developed and implemented a complete contract with over 300 employees
+
+
+Educación: 
+M.A. Hsg In Marketing, Services And Communication Management
+University Of St. Gallen (Hsg)
+2010 - 2014
+·Graduated with a First Class Honours, Top 5% of the cohort
+·Studied abroad and studied about the school with a first class honors
+
+*/
