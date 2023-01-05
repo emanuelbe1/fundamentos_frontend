99377abcf6f72470442c51791f1f10abc8ddb5d5
Emanuel B <emanuelbe3@gmail.com>
2023-01-05 12:40:35 -0300

    etc

--- 5-Fundamentos_JavaScript/js/function2.js
+++ 5-Fundamentos_JavaScript/js/function2.js
@@ -1,4 +1,27 @@
-document.getElementById("boton").onclick = function(){
+/**document.getElementById("boton").onclick = function(){
     console.log("Capturamos el evento click");
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
-}
+}*/
+//Al evento, agregarle un evento listener. Se puede conocer más eventos en
+//HTML DOM event w3schools.
+
+
+/*document.addEventListener('click', function(){
+    console.log("Hola desde EventListener");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+});*/
+
+document.getElementById("boton_color_r").addEventListener('click', function() {
+    console.log("Capturamos el evento click para cambiar el fondo");
+    document.body.style.backgroundColor = 'rgb(75, 118, 217)';
+});//explicación: cuando haya un click en el boton color, se va a cambiar el fondo
+
+document.getElementById("boton_color_l").addEventListener('click', function() {
+    console.log("Capturamos el evento click para cambiar el fondo a lightsteel blue");
+    document.body.style.backgroundColor = '#D1C7C5';
+});
+
+document.getElementById("boton_default").addEventListener('click', function() {
+    console.log("Capturamos el evento click para cambiar el fondo a default");
+    document.body.style.backgroundColor = 'wheat';
+});
