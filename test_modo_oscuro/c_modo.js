19242e1dae07032bf328e81db88941f5dabb9da5
Emanuel B <emanuelbe3@gmail.com>
2023-01-09 22:37:26 -0300

    Creada función para modo oscuro

--- test_modo_oscuro/c_modo.js
+++ test_modo_oscuro/c_modo.js
@@ -1,6 +1,5 @@
-document.getElementById('pagestyle').setAttribute('href', pagesheet)
-
-document.getElementById("boton_ocultar2").addEventListener('click', function() {
+/*document.getElementById('pagestyle').setAttribute('href', pagesheet)
+/*document.getElementById("b_modo").addEventListener('click', function() {
     if(document.getElementById('exp2').style.display == 'none'){
         document.getElementById('exp2').style.display = 'initial';
         document.getElementById('boton_ocultar2').innerHTML = 'Ocultar'
@@ -9,4 +8,14 @@ document.getElementById("boton_ocultar2").addEventListener('click', function() {
         document.getElementById('exp2').style.display = 'none';
         document.getElementById('boton_ocultar2').innerHTML = 'Mostrar detalles'
     };
+});*/
+
+
+document.getElementById("b_modo").addEventListener('click', function() {
+    if(document.getElementById('estilo').getAttribute('href') == "m_oscuro.css" ){
+        document.getElementById('estilo').setAttribute('href', "m_claro.css");
+    }else{
+        document.getElementById('estilo').setAttribute('href', "m_oscuro.css");
+        console.log("Se cambio al color oscuro.");
+    }
 });
