d294d5a3cf7634f45be7385769d5e383270f7062
Emanuel B <emanuelbe3@gmail.com>
2023-01-27 13:09:27 -0300

    nuevos subdirectorios y archivos

---
+++ p_javaScript/juego_memorizar/memoria.js
@@ -0,0 +1,12 @@
+/*
+En este proyecto se van a cubrir:
+ · push()
+ · querySelector()
+ · setAttribute()
+ · getAttribute()
+ · createElement()
+ · appendChild()
+ · Math.random()
+ · sort()
+ · For loops
+*/ var
