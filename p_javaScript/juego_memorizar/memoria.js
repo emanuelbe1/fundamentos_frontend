ad2c4d37ce209d41828016d75e78f31c7ee23ae5
Emanuel B <emanuelbe3@gmail.com>
2023-01-27 14:07:22 -0300

    creando juego de memoria

--- p_javaScript/juego_memorizar/memoria.js
+++ p_javaScript/juego_memorizar/memoria.js
@@ -9,4 +9,76 @@ En este proyecto se van a cubrir:
  · Math.random()
  · sort()
  · For loops
-*/ var
+*/ 
+document.addEventListener("DOMContentLoaded", () =>{
+    //CARD OPTIONS: Seis comidas (Test)
+    const cardArray = [
+        {
+            name: 'helado',
+            img: 'imagenes/helado.png'
+        },
+        {
+            name: 'helado',
+            img: 'imagenes/helado.png'
+        },
+        {
+            name: 'huevos',
+            img: 'imagenes/huevos.png'
+        },
+        {
+            name: 'huevos',
+            img: 'imagenes/huevos.png'
+        },
+        {
+            name: 'leche',
+            img: 'imagenes/leche.png'
+        },
+        {
+            name: 'leche',
+            img: 'imagenes/leche.png'
+        },
+        {
+            name: 'papas_fritas',
+            img: 'imagenes/papas_fritas.png'
+        },
+        {
+            name: 'papas_fritas',
+            img: 'imagenes/papas_fritas.png'
+        },
+        {
+            name: 'pez',
+            img: 'imagenes/pez.png'
+        },
+        {
+            name: 'pez',
+            img: 'imagenes/pez.png'
+        },
+        {
+            name: 'pizza',
+            img: 'imagenes/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'imagenes/pizza.png'
+        }
+    ]
+    const grid = document.querySelector('.grid')
+    //creamos nuestro tablero
+    function createBoard(){
+        for(let i = 0; i < cardArray.length; i++){
+            var card= document.createElement('img')
+            card.setAttribute('src', 'imagenes/blank.jpg')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click',flipcard)
+            grid.appendChild(card)
+    }
+}
+
+
+createBoard()
+
+
+
+
+
+})
