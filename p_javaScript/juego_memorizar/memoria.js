12d0c63bb258faeab7b626434580ddf7f0352f03
Emanuel B <emanuelbe3@gmail.com>
2023-01-27 16:36:43 -0300

    codigo y archivos

--- p_javaScript/juego_memorizar/memoria.js
+++ p_javaScript/juego_memorizar/memoria.js
@@ -62,21 +62,62 @@ document.addEventListener("DOMContentLoaded", () =>{
             img: 'imagenes/pizza.png'
         }
     ]
-    const grid = document.querySelector('.grid')
+
+cardArray.sort(() => 0.5 - Math.random())
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen =[]
+var cardsChosenId=[]
+var CardsWon = []
     //creamos nuestro tablero
-    function createBoard(){
-        for(let i = 0; i < cardArray.length; i++){
-            var card= document.createElement('img')
-            card.setAttribute('src', 'imagenes/blank.jpg')
-            card.setAttribute('data-id', i)
-            //card.addEventListener('click',flipcard)
-            grid.appendChild(card)
+function createBoard(){
+    for(let i = 0; i < cardArray.length; i++){
+        var card= document.createElement('img')
+        card.setAttribute('src', 'imagenes/blank.jpg')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click',flipCard)
+        grid.appendChild(card)
+    }
+}
+//comprobar coincidencia
+function checkForMatch(){
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]){
+        alert('Has encontrado una coincidencia!')
+        cards[optionOneId].setAttribute('src', 'imagenes/white.png')
+        cards[optionTwoId].setAttribute('src', 'imagenes/white.png')
+        cardsWon.push(cardsChosen)
+    }else{
+        cards[optionOneId].setAttribute('src', 'imagenes/blank.jpg')
+        cards[optionTwoId].setAttribute('src', 'imagenes/blank.jpg')
+        alert('Lo siento, intenta de nuevo')
+    }
+    cardsChosen =[]
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon === cardArray.length/2){
+        resultDisplayDisplay.textContent = 'Felicidades! has ganado!'
+    }
+}
+
+//voltear nuestra tarjeta
+function flipCard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length===2){
+        setTimeout(checkForMatch, 500)
     }
 }
 
 
-createBoard()
 
+
+createBoard()
+ 
 
 
 
